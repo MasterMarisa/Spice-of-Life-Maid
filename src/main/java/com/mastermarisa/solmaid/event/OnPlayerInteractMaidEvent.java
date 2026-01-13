631062b0016c3490e2ae4b2df5b03bc18e5073aa
摸里傻 <147955526+MasterMarisa@users.noreply.github.com>
2026-01-13 20:43:40 +0800

    Add files via upload

---
+++ src/main/java/com/mastermarisa/solmaid/event/OnPlayerInteractMaidEvent.java
@@ -0,0 +1,29 @@
+package com.mastermarisa.solmaid.event;
+
+import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import com.mastermarisa.solmaid.ui.FoodScreen;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+
+@OnlyIn(Dist.CLIENT)
+public class OnPlayerInteractMaidEvent {
+    @SubscribeEvent
+    public static void onPlayerInteractMaidEvent(PlayerInteractEvent.EntityInteract event){
+        if(!event.getLevel().isClientSide()){
+            return;
+        }
+        Player player = event.getEntity();
+        Entity entity = event.getTarget();
+        if(entity instanceof EntityMaid maid){
+            if(player.isSecondaryUseActive() && player.getMainHandItem().is(Items.BOOK)){
+                FoodScreen.open(player,maid);
+                event.setCanceled(true);
+            }
+        }
+    }
+}
