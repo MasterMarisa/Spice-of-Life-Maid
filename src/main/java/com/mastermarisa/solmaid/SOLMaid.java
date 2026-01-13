cd98e1de1e20fdea1f5bb7573e7630e62929a225
摸里傻 <147955526+MasterMarisa@users.noreply.github.com>
2026-01-13 20:40:14 +0800

    Create SOLMaid.java

---
+++ src/main/java/com/mastermarisa/solmaid/SOLMaid.java
@@ -0,0 +1,39 @@
+package com.mastermarisa.solmaid;
+
+import com.mastermarisa.solmaid.config.ModConfig;
+import com.mastermarisa.solmaid.event.*;
+import com.mastermarisa.solmaid.registry.ModAttachmentTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(SOLMaid.MOD_ID)
+public class SOLMaid {
+    public static final String MOD_ID = "solmaid";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static ResourceLocation resourceLocation(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+    }
+
+    public SOLMaid(IEventBus modEventBus, ModContainer modContainer, Dist dist) {
+        ModAttachmentTypes.ATTACHMENT_TYPES.register(modEventBus);
+
+        NeoForge.EVENT_BUS.register(OnMaidFoodEatenEvent.class);
+        NeoForge.EVENT_BUS.register(OnMaidJoinWorldEvent.class);
+        NeoForge.EVENT_BUS.register(OnPlayerInteractMaidEvent.class);
+        if(OnAddJadeInfoEvent.JADE_LOADED){
+            NeoForge.EVENT_BUS.register(OnAddJadeInfoEvent.class);
+        }
+
+        modContainer.registerConfig(net.neoforged.fml.config.ModConfig.Type.SERVER, ModConfig.SPEC);
+        modContainer.registerConfig(net.neoforged.fml.config.ModConfig.Type.CLIENT, ModConfig.CLIENT_SPEC);
+    }
+}
